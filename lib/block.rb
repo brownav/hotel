25e5e3edbfd292a4e32c386336076eb052e96e8f
Alexandria Brown <axbn2190@gmail.com>
2018-04-01 17:09:55 -0700

    added block class and block class testing

---
+++ lib/block.rb
@@ -0,0 +1,23 @@
+require 'date'
+require 'pry'
+
+module Hotel
+
+  class Block
+    attr_reader :id, :dates, :open_rooms, :booked_rooms
+
+    def initialize(block)
+      @id = block[:id]
+      @dates = block[:dates]
+      @open_rooms = block[:open_rooms]
+      @booked_rooms = block[:booked_rooms]
+    end
+
+    def book_room
+      @booked_rooms << @open_rooms.pop
+    end
+
+  end
+
+
+end
---
+++ specs/block_spec.rb
@@ -0,0 +1,36 @@
+require_relative 'spec_helper'
+
+
+describe "Block class" do
+  before do
+    @block = Hotel::Block.new(id: 1, dates: ["12 May", "13 May", "14 May"], open_rooms: [1, 2, 3], booked_rooms: [])
+
+    describe "initializer" do
+      it "is an instance of block" do
+        @block.must_be_kind_of Hotel::Block
+      end
+
+      it "is set up for specific attributes and data types" do
+        [:id, :dates, :open_rooms, :booked_rooms].each do |prop|
+          @block.must_respond_to prop
+        end
+
+        @block.id.must_be_kind_of Integer
+        @block.dates.must_be_kind_of Array
+        @block.dates.first.must_be_kind_of String
+        @block.open_rooms.must_be_kind_of Array
+        @block.open_rooms.first.must_be_kind_of Integer
+        @block.booked_rooms.must_be_kind_of Array
+      end
+    end
+
+    describe "book_room method" do
+      initial_open_rooms = @block.open_rooms
+
+      @block.book_room
+
+      @block.open_rooms.must_equal initial_open_rooms.first(2)
+      @block.booked_rooms.must_equal initial_open_rooms.last
+    end
+  end
+end
